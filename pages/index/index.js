2fdc50e6579c45fb044b256d9f4f75aac71a90b5
2218675712 <2218675712@qq.com>
2020-09-01 12:00:02 +0800

    2020年09月01日 11:59:50 轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,18 @@
 Page({
+    data: {
+        // 轮播图数组
+        swiperList: []
+    },
+    // 页面开始加载触发事件
+    onLoad() {
+        wx.request({
+            url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata\n',
+            success:(result)=>{
+                this.setData({
+                    swiperList:result.data.message
+                })
+            }
 
+        })
+    }
 })
