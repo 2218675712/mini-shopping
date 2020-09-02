8c5c9bba5b687bb10a5da098d27571573d4611b3
2218675712 <2218675712@qq.com>
2020-09-02 21:28:32 +0800

    2020年09月02日 21:28:22 分类页面布局

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,6 +1,39 @@
+import {request} from '../../request/index.js';
+
 Page({
-    data: {},
+    data: {
+        // 左侧菜单数据
+        leftMenuList: [],
+        // 右侧商品数据
+        rightContent: [],
+        // 被点击的左侧菜单
+        currentIndex:0
+
+    },
+    // 接口返回数据
+    Cate: [],
     onLoad: function (options) {
+        this.getCate()
+    },
+    /**
+     * 获取分类数据
+     */
+    getCate() {
+        request({
+            url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+        }).then((res) => {
+            this.Cate = res.data.message
+            // 构造左侧菜单数据
+            let leftMenuList = this.Cate.map(v => v.cat_name)
+            // 构造右侧商品数据
+            let rightContent = this.Cate[0].children;
+            this.setData({
+                leftMenuList,
+                rightContent
+            })
+
+
+        })
 
     }
 });
