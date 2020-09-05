879864de8ce1a2e5be55f063645b95de70765bcc
2218675712 <2218675712@qq.com>
2020-09-05 10:55:52 +0800

    2020年09月05日 10:55:43 获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,10 +3,32 @@ Page({
     onLoad: function (options) {
 
     },
+    /**
+     * 获取收货地址
+     */
     handleChooseAddress() {
-        wx.chooseAddress({
+        // 获取用户授权设置
+        wx.getSetting({
             success: (result) => {
-                console.log(result)
+                // 获取收货地址授权状态
+                let scopeAddress = result.authSetting['scope.address']
+                // 如果是成功或者未定义,则获取收货地址
+                if (scopeAddress === true || scopeAddress === undefined) {
+                    wx.chooseAddress({
+                        success: (result1) => {
+                        }
+                    })
+                } else {
+                    // 取消状态,调用已经授权的权限,让用户重新授权
+                    wx.openSetting({
+                        success: (result2) => {
+                            wx.chooseAddress({
+                                success: (result3) => {
+                                }
+                            })
+                        }
+                    })
+                }
             }
         })
     }
