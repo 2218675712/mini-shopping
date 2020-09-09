0d07797dce0dc34dab5333604fdcbdfc5267c8aa
2218675712 <2218675712@qq.com>
2020-09-09 22:00:00 +0800

    2020年09月09日 21:59:47 搜索中心

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,6 +1,39 @@
+import {request} from "../../request/index";
+
 Page({
-    data: {},
+    data: {
+        goods: []
+    },
     onLoad: function (options) {
 
+    },
+    TimeId: -1,
+    /**
+     * 输入框值改变
+     * @param e
+     */
+    handleInput(e) {
+        // 获取输入框的值
+        const {value} = e.detail
+        // 检测合法性
+        if (!value.trim()) {
+            // 值不合法
+            return
+        }
+        // 防抖实现
+        // 准备发送请求获取数据
+        clearTimeout(this.TimeId)
+        this.TimeId = setTimeout(() => {
+            this.qsearch(value)
+        }, 1000)
+    },
+    async qsearch(query) {
+        const res = await request({
+            url: '/goods/qsearch',
+            data: {query}
+        })
+        this.setData({
+            goods: res
+        })
     }
 });
