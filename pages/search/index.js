1e9c909b746edab2a50f30c1adca87caa66d81bc
2218675712 <2218675712@qq.com>
2020-09-10 11:13:25 +0800

    取消按钮功能实现

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,7 +2,10 @@ import {request} from "../../request/index";
 
 Page({
     data: {
-        goods: []
+        goods: [],
+        // 取消按钮是否显示
+        isFocus: false,
+        inputValue: ''
     },
     onLoad: function (options) {
 
@@ -17,16 +20,36 @@ Page({
         const {value} = e.detail
         // 检测合法性
         if (!value.trim()) {
+            this.setData({
+                goods: [],
+                isFocus: false
+            })
             // 值不合法
             return
         }
+        // 取消按钮显示
+        this.setData({
+            isFocus: true
+        })
         // 防抖实现
         // 准备发送请求获取数据
+
+        /*
+        * 防抖（防止抖动）定时器节流
+        * 防抖一般输入框中防止重复输入重复发送请求
+        * 节流一般是用在页面下拉和上拉
+        * 定义全局的定时器id
+        * */
         clearTimeout(this.TimeId)
         this.TimeId = setTimeout(() => {
             this.qsearch(value)
         }, 1000)
     },
+    /**
+     * 搜索内容
+     * @param query 内容
+     * @returns {Promise<void>}
+     */
     async qsearch(query) {
         const res = await request({
             url: '/goods/qsearch',
@@ -34,6 +57,13 @@ Page({
         })
         this.setData({
             goods: res
+        })
+    },
+    handleCancel() {
+        this.setData({
+            goods: [],
+            isFocus: false,
+            inputValue: ''
         })
     }
 });
