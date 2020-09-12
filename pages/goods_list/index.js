ed9944d7ebfa678260decec0b287d27bfa1a0a88
2218675712 <2218675712@qq.com>
2020-09-12 11:57:09 +0800

    完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -33,7 +33,8 @@ Page({
     totalPages: 1,
     // 监听事件加载
     onLoad: function (options) {
-        this.QueryParams.cid = options.cid
+        this.QueryParams.cid = options.cid||""
+        this.QueryParams.query = options.query||""
         this.getGoodsList()
 
     },
